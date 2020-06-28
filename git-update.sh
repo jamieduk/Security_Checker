278d9f04c11baff18449b8947b91562171b9604d
Jay <jamied_uk@hotmail.com>
2020-06-28 15:30:51 +0100

    Update git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-#echo "# Security_Checker" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/jamieduk/Security_Checker.git
-git push -u origin master
+
