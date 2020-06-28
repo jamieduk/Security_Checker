af4cf1fc7e7f97b5db9f4e6a612c657dbc6bfe72
Jay <jamied_uk@hotmail.com>
2020-06-28 15:38:20 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo "Starting Update, Please Wait...."
 ppp=$('pwd')
 git clone https://github.com/jamieduk/Security_Checker.git
-mv Security_Checker/* $ppp -f
-sudo rm Security_Checker
+mv Security_Checker/* $ppp -f -y
+sudo rm -r Security_Checker
 echo "All Done."
